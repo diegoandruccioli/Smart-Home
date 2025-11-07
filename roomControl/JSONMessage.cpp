4f4f9abfa59abc04ff93a88bd844a832411759a6
Diego Andruccioli <diego.andruccioli@studio.unibo.it>
2025-11-07 21:48:48 +0100

    Logic: Add Msg class for JSON deserialization (ArduinoJson wrapper).

---
+++ roomControl/JSONMessage.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include <ArduinoJson.h>
+
+// Classe semplice per messaggi JSON
+class Msg {
+private:
+  String content;
+  StaticJsonDocument<128> doc;
+
+public:
+  // Costruttore: riceve stringa JSON e deserializza
+  Msg(String content) : content(content) {
+    deserializeJson(doc, content);
+  }
+
+  // Restituisce contenuto JSON originale
+  String getContent() {
+    return content;
+  }
+
+  // Estrae il campo "name" dal JSON
+  String getSensorName() {
+    return doc["name"].as<String>();
+  }
+
+  // Estrae il campo "timestamp" dal JSON
+  long getTimestamp() {
+    return doc["timestamp"].as<long>();
+  }
+
+  // Estrae il campo "measure" dal JSON
+  int getMeasure() {
+    return doc["measure"].as<int>();
+  }
+};
